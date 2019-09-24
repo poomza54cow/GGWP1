10185b584b617ca03c2e6190318a09205aba421f
poomza54cow <52213368+poomza54cow@users.noreply.github.com>
2019-09-24 09:00:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,214 @@
 require('dotenv').config();
+const request = require('request');
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3001;
 const _ = require('lodash');
-
-
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// STATUS LED
+let status = [false, false];
+// TOPIC
+const LED_TOPIC = `/ESP/LED`;
 // Create a MQTT Client
 const mqtt = require('mqtt');
-// Firebase init
-const admin = require("firebase-admin");
-// Fetch the service account key JSON file contents
-let serviceAccount = require("./config/serviceAccountKey.json");
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://nodemcu-a3f6b.firebaseio.com"
-});
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-const db = admin.database();
-const ref = db.ref("temperature");
-
 // Create a client connection to CloudMQTT for live data
-const client = mqtt.connect('mqtt://soldier.cloudmqtt.com', {
-  username: 'uumiczri',
-  password: '4bF40hfXlXH6',
-  port: 13043
+const client = mqtt.connect('soldier.cloudmqtt.com',  // Server MQTT ของเรานะ
+{
+  username: 'uumiczri', // Username MQTT ของเรานะ
+  password: '4bF40hfXlXH6', // Password MQTT ของเรานะ
+  port: xxxxx // Port MQTT ของเรานะ
 });
-
 client.on('connect', function() { 
   // When connected
   console.log("Connected to CloudMQTT");
-  // Subscribe to the temperature
-  client.subscribe('/ESP/TEMP', function() {
-  });
-
-  client.subscribe('/ESP/RELAY', function() {
+client.subscribe('/ESP/LED', function() {
     // when a message arrives, do something with it
     client.on('message', function(topic, message, packet) {
-      if (topic == '/ESP/RELAY') {
-        let now = new Date().valueOf();
-        if (message.indexOf('1_') > -1) {
-          if (message == '1_ON') {
-            let usersRef = db.ref("relay").update({ 1: true });
-          } else {
-            let usersRef = db.ref("relay").update({ 1: false });
+      switch(topic) {
+        case LED_TOPIC:
+          messageFromBuffer = message.toString('utf8');
+          if (messageFromBuffer != 'GET') {
+            const splitStatus = messageFromBuffer.split(',');
+            if (splitStatus.length > 0) {
+              splitStatus.map((ele, index)=> {
+                console.log(`DOIT ${ele} ${index} ${parseInt(ele)}`);
+                if (ele == 0) {
+                  status[index] = false;
+                } else {
+                  status[index] = true;
+                }
+              });
+            }
           }
-        }
-
-        if (message.indexOf('2_') > -1) {
-          if (message == '2_ON') {
-            let usersRef = db.ref("relay").update({ 2: true });
-          } else {
-            let usersRef = db.ref("relay").update({ 2: false });
-          }
-        }
+console.log(`Received '${message}' on '${topic}`);
+        break;
+        default:
+          console.log(`Unknow Topic group`);
       }
-
-      if (topic == '/ESP/TEMP') {
-        let now = new Date().valueOf();
-        let usersRef = db.ref("temperature").child(now);
-        usersRef.set({
-          topic: topic,
-          temp:  parseFloat(message),
-          createAt: new Date().toLocaleString('en-US', {
-            timeZone: 'Asia/Bangkok'
-          }),
-          timestamp: now
-        });
-      }
-
-      console.log("Received '" + message + "' on '" + topic + "'");
-
-
     });
   });
 });
-
+app.post('/webhook', async (req, res) => {
+const message = req.body.events[0].message.text;
+  const reply_token = req.body.events[0].replyToken;
+  const TOKEN = `sBzunhVMVgT+ACb/gGCBEO2gjMFsudGLaF3ZXSAgXnLMsEkvN393Jb9Fs7kJ4O9s6aulil89P0svUj8xKho7w3s0803QXtMQkHUvFEALxYw7JaiXiMc7mJNoa2nt2CrjMx88vM1iUmgAaKO1kbBcugdB04t89/1O/w1cDnyilFU=x`; // Token ที่ได้จาก Channel access token
+  const HEADERS = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${TOKEN}`
+  };
+if (message == 'เปิดไฟ หน้าบ้าน' || message == 'ปิดไฟ หน้าบ้าน') {
+    if (message == 'เปิดไฟ หน้าบ้าน') {
+      await mqttMessage(LED_TOPIC, 'LEDON_ONE');
+    } else {
+      await mqttMessage(LED_TOPIC, 'LEDOFF_ONE');
+    }
+  }
+if (message == 'เปิดไฟ หลังบ้าน' || message == 'ปิดไฟ หลังบ้าน') {
+    if (message == 'เปิดไฟ หลังบ้าน') {
+      await mqttMessage(LED_TOPIC, 'LEDON_TWO');
+    } else {
+      await mqttMessage(LED_TOPIC, 'LEDOFF_TWO');
+    }
+  }
+mqttMessage(LED_TOPIC, 'GET');
+if (message == 'สถานะทั้งหมด') {
+    await checkStatus();
+  } else {
+    await checkStatus();
+  }
+console.log(status);
+  const objectMessage = genFlexMessage(status[0], status[1]);
+const body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      objectMessage
+    ]
+  });
+request({
+    method: `POST`,
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers: HEADERS,
+    body: body
+  });
+res.sendStatus(200);
+});
+let mqttMessage = async (topic, message) => {
+  client.publish(topic, message);
+  await checkStatus();
+}
+let checkStatus = async () => {
+  await new Promise(done => setTimeout(done, 3000));
+}
+let genFlexMessage = (ledOne, ledTwo) => {
+  return {
+    "type": "flex",
+    "altText": "สถานะระบบไฟ",
+    "contents": {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://www.ihome108.com/wp-content/uploads/2017/05/home-slide-01.jpg",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "label": "Line",
+          "uri": "https://linecorp.com/"
+        }
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "ระบบไฟ",
+            "flex": 0,
+            "size": "xl",
+            "weight": "bold"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "flex": 1,
+            "margin": "md",
+            "contents": [
+              {
+                "type": "text",
+                "text": "ไฟหน้าบ้าน",
+                "align": "start",
+                "gravity": "top",
+                "weight": "bold"
+              },
+              {
+                "type": "text",
+                "text": (ledOne == true) ? "Open" : "Close",
+                "align": "start",
+                "weight": "bold",
+                "color": (ledOne == true) ? "#FF0000" : "#000000",
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "flex": 1,
+            "margin": "md",
+            "contents": [
+              {
+                "type": "text",
+                "text": "ไฟหลังบ้าน",
+                "align": "start",
+                "gravity": "top",
+                "weight": "bold"
+              },
+              {
+                "type": "text",
+                "text": (ledTwo == true) ? "Open" : "Close",
+                "align": "start",
+                "weight": "bold",
+                "color": (ledTwo == true) ? "#FF0000" : "#000000",
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "flex": 0,
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": `${(ledOne == true) ? "ปิดไฟ" : "เปิดไฟ"}หน้าบ้าน`,
+              "text": `${(ledOne == true) ? "ปิดไฟ" : "เปิดไฟ"} หน้าบ้าน`
+            },
+            "height": "sm",
+            "style": "link"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": `${(ledTwo == true) ? "ปิดไฟ" : "เปิดไฟ"}หลังบ้าน`,
+              "text": `${(ledTwo == true) ? "ปิดไฟ" : "เปิดไฟ"} หลังบ้าน`
+            },
+            "height": "sm",
+            "style": "link"
+          },
+          {
+            "type": "spacer",
+            "size": "sm"
+          }
+        ]
+      }
+    }
+  };
+}
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
